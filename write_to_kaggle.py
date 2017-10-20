178a3169c415942e755cde39ce38cab6233a196a
Seara <searachen@gmail.com>
2017-10-20 15:17:41 -0400

    Create write_to_kaggle.py

---
+++ write_to_kaggle.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def write_to_kaggle(y_pred,directory):
+	a=np.array(y_pred)
+	df= pd.DataFrame(data=a, columns=['Category'])
+	df.index.name='Id'
+	df.to_csv(directory, sep=',')
