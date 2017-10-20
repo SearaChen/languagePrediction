381780e9fc84fd7c9526c5a18cd0ef2d7381eab1
Seara <searachen@gmail.com>
2017-10-20 15:18:59 -0400

    Update write_to_kaggle.py

--- write_to_kaggle.py
+++ write_to_kaggle.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+
+'''
+accept a list of numbers which indicates your class classification 
+'''
 def write_to_kaggle(y_pred,directory):
 	a=np.array(y_pred)
 	df= pd.DataFrame(data=a, columns=['Category'])
